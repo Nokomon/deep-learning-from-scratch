050dee1e2473c4b8673cbb591a31b69b7f3e66f9
Nokomon <drnurimbo@gmail.com>
2022-01-16 17:46:53 +0900

    220116

--- scratch.py
+++ scratch.py
@@ -1,6 +1,10 @@
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+import numpy as np
 
-a = stopwords.words('english')
-print(a[:5])
+you = list(map(float, "-0.9031807 -1.0374491 -1.468 -1.321 0.931".split()))
+i = list(map(float, "-1.06 -0.91 -0.31 -0.57 1.04".split()))
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+you = np.array(you).reshape(1, -1)
+i = np.array(i).reshape(1, -1)
+print(cosine_similarity(you, i))
