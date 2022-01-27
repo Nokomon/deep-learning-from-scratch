fix issues(regarding softmax) in lstm

common/functions.py
@@ -21,4 +21,7 @@ def cross_entropy_error(y, t):
 
     batch_size = y.shape[0]
 
-    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size\
+
+def sigmoid(x):
+    return 1/ (1 + np.exp(-x))